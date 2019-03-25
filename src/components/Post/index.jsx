9d123afe531c70d36f496bfef3e9beb2c41bb1b7
Jo-GyuHyeon <kuhyun1993@naver.com>
2019-03-25 16:08:51 +0900

    added: post toc  - scroll spy  - scroll smooth

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -18,31 +18,44 @@ class Post extends React.Component {
 
     return (
       <div className="post">
-        <div className="post__meta">
-          <time
-            className="post__meta-time"
-            dateTime={moment(date).format('MMMM D, YYYY')}
-          >
-            {moment(date).format('MMMM YYYY')}
-          </time>
-          <span className="post__meta-divider" />
-          <span className="post__meta-category" key={categorySlug}>
-            <Link to={categorySlug} className="post__meta-category-link">
-              {category}
-            </Link>
-          </span>
-        </div>
-        <h2 className="post__title">
+
+        <div className="post__thumbnail">
           <Link className="post__title-link" to={slug}>
-            {title}
             <img src={thumbail} />
           </Link>
-        </h2>
+        </div>
 
-        <p className="post__description">{description}</p>
-        <Link className="post__readmore" to={slug}>
-          Read
-        </Link>
+        <div className="post__content">
+          <h2 className="post__content__title">
+            <Link className="post__content__title-link" to={slug}>
+              {title}
+            </Link>
+          </h2>
+
+          <div className="post__content__meta">
+            <time
+              className="post__content__meta-time"
+              dateTime={moment(date).format('MMMM D, YYYY')}
+            >
+              {moment(date).format('YYYY년 MM월 DD일 hh:mm')}
+            </time>
+            <span className="post__content__meta-divider" />
+            <span className="post__content__meta-category" key={categorySlug}>
+              <Link to={categorySlug} className="post__meta-category-link">
+                {category}
+              </Link>
+            </span>
+          </div>
+          <p className="post__description">{description}</p>
+        </div>
+        {
+          /* 
+          <Link className="post__readmore" to={slug}>
+            Read
+           </Link> 
+           */
+        }
+
       </div>
     )
   }
--- src/components/PostTemplateDetails/index.jsx
+++ src/components/PostTemplateDetails/index.jsx
@@ -1,13 +1,75 @@
 import React from 'react'
+import kebabCase from 'lodash/kebabCase'
+import throttle from 'lodash/throttle';
 import { Link } from 'gatsby'
 import moment from 'moment'
 import Disqus from '../Disqus/Disqus'
 import './style.scss'
 
 class PostTemplateDetails extends React.Component {
+
+  tocHeader = [];
+  postHeader = [];
+
+  componentDidMount() {
+    this.registerEvent();
+    this.setPostHeaderId();
+    this.getHeaders();
+  }
+
+  componentWillUnmount() {
+    this.unregisterEvent();
+  }
+
+  setPostHeaderId = () => {
+    const headers = document.body.querySelectorAll('.post-single__body > h1, h2, h3, h4, h5, h6')
+    headers.forEach(header => {
+      const id = kebabCase(header.innerText)
+      header.setAttribute('id', id)
+    })
+  }
+
+  getHeaders = () => {
+    const toc = document.body.querySelectorAll('.post-single__table_of_contents > ul > li')
+    const headers = document.body.querySelectorAll('.post-single__body > h1, h2, h3, h4, h5, h6')
+    this.tocHeader = toc;
+    this.postHeader = headers;
+  }
+
+  onScroll = throttle(() => {
+    const scrollTop = this.getScrollTop();
+    Array.from(this.postHeader).forEach((header, index) => {
+      if (scrollTop >= header.offsetTop) {
+        const prev_active_header = document.body.querySelector('.active')
+        if (prev_active_header) {
+          prev_active_header.removeAttribute('class')
+        }
+        this.tocHeader[index].setAttribute('class', 'active')
+      }
+    })
+  }, 250);
+
+  getScrollTop = () => {
+    if (!document.body) return 0;
+    const scrollTop = document.documentElement
+      ? document.documentElement.scrollTop || document.body.scrollTop
+      : document.body.scrollTop;
+    return scrollTop;
+  };
+
+  registerEvent = () => {
+    window.addEventListener('scroll', this.onScroll);
+  };
+
+  unregisterEvent = () => {
+    window.removeEventListener('scroll', this.onScroll);
+  };
+
+
   render() {
     const { subtitle, author } = this.props.data.site.siteMetadata
     const post = this.props.data.markdownRemark
+    const { tableOfContents } = post
     const tags = post.fields.tagSlugs
 
     const homeBlock = (
@@ -45,6 +107,8 @@ class PostTemplateDetails extends React.Component {
     return (
       <div>
         {homeBlock}
+        <div className="post-single__table_of_contents" dangerouslySetInnerHTML={{ __html: tableOfContents }} />
+
         <div className="post-single">
           <div className="post-single__inner">
             <h1 className="post-single__title">{post.frontmatter.title}</h1>
