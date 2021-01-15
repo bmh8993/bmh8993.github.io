ca7a347facaf5081eb5e31e8326170abac647311
MinHyeokBae <bmh9175@gmail.com>
2021-01-15 17:38:27 +0900

    add seo

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -9,10 +9,17 @@ class Layout extends React.Component {
 
     return (
       <div className="layout">
-        <Helmet defaultTitle="Regyu Dev log">
+        <Helmet defaultTitle="Blog by 인간지능">
           <link rel="shortcut icon" href={favicon} />
-          <meta name="google-site-verification" content="t_49cP6QwzmvUiZaME8a6LXRipkHQGiYSoruZ_PSlEc" />
-          <meta name="naver-site-verification" content="61e42118f4c8c878479a02a76a96cf21191ee4bc" />
+          <meta
+            name="google-site-verification"
+            content="swq3G9HU89k-urr7vJ5yC-tBFErjxq2ePSgOf0UQ-Tc"
+          />
+
+          <meta
+            name="naver-site-verification"
+            content="77a79e7a3b57fba2737e27fcbfe8e40aebd22d35"
+          />
         </Helmet>{' '}
         {children}{' '}
       </div>
